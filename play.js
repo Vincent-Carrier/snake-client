1505330db205cda12e64c4222cd6c08ccf1d1415
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-04 13:37:53 -0500

    Complete move commands assignment

--- play.js
+++ play.js
@@ -1,6 +1,20 @@
-const { connect } = require('./client');
+const { connect } = require("./client");
 
-console.log('Connecting ...');
-connect().on('data', data => {
+console.log("Connecting ...");
+const conn = connect();
+
+conn.on("data", data => {
   console.log("u ded");
 });
+
+// conn.on("connect", () => {
+//   for (let i = 0; i < 90; i++) {
+//     setTimeout(
+//       () =>
+//         conn.write("Move: up", err => {
+//           if (err) throw "Oops";
+//         }),
+//       50 * i
+//     );
+//   }
+// });
