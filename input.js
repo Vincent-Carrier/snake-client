b49760677451fb6014c8b7e4896fb22f7a34c2a5
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-04 15:08:27 -0500

    Add messaging functionality

--- input.js
+++ input.js
@@ -1,24 +1,38 @@
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
+  stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
-}
+};
 
 const stdin = setupInput();
 
 const keybindings = {
-  'w': 'up',
-  'a': 'left',
-  's': 'down',
-  'd': 'right',
+  w: "up",
+  a: "left",
+  s: "down",
+  d: "right"
+};
+
+const messages = {
+  l: "lol",
+  r: "rofl"
 };
 
 const handleUserInput = function(conn) {
-  stdin.on('data', char => {
-    if (char === '') process.exit;
-    conn.write(`Move: ${keybindings[char]}`, err => { if (err) throw err; })
+  stdin.on("data", char => {
+    if (char === "q") process.exit;
+    if (keybindings[char]) {
+      conn.write(`Move: ${keybindings[char]}`, err => {
+        if (err) throw err;
+      });
+    } 
+    if (messages[char]) {
+      conn.write(`Say: ${messages[char]}`, err => {
+        if (err) throw err;
+      });
+    }
   });
 };
 
